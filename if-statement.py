c4409588ae547e1e373421091b62f1e4041031c8
Aluda-Ian <ianaluda27@students.uonbi.ac.ke>
2024-03-01 23:02:56 +0300

    second commit

---
+++ if-statement.py
@@ -0,0 +1,20 @@
+#num = 20
+#if num >= 20 :
+    #print("the number is equal or greater than 20")
+
+    #the if condition evaluates the conditon 
+    #conditional statements in python include if statement , if elsestatement and if elif statement.
+
+#online shoping 
+
+bill = 200
+
+discount1 = 100
+
+if bill > 2000:
+    print('bill is greater than 2000')
+    bill =  bill - discount1
+else:
+    print("bill is less than 2000")    
+
+    print("final bill " + str(bill))
