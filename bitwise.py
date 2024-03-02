5f6c7a8794e7e183e7cd6c122ace60e213cd932e
Aluda-Ian <ianaluda27@students.uonbi.ac.ke>
2024-03-02 09:28:37 +0300

    you wii use pyth0n syntax

---
+++ bitwise.py
@@ -0,0 +1,20 @@
+#bitwise operators or, and , xor , nand
+
+#Mum : father = all parents are hoem
+#True or True = true
+# true or false = true
+#true and true = true
+#true and false = false
+
+b1 = True
+b2 = True
+b3 = False
+
+print(b1 or b2) #true
+print(b1 or b3) #true
+
+
+#and 
+
+print (b1 and b2 ) #true
+print (b1 and b3)  #false
